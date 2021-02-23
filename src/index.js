2b66d6a4b053468c49e3fe5dc51566c936fbaf51
Maxim Tsatsura <maxim.tsatsura@gmail.com>
2021-02-23 16:06:02 +0300

    feat: add subtask towel-sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+  if (!matrix) return [];
+  if (!Array.isArray(matrix)) return [];
+  if (matrix.length == 0) return [];
+
+    let resultArr = matrix.map( (item, index) => {
+      if (index%2 == 0) {
+     
+      item = item.sort((a,b) => {return a-b});} else { 
+      item = item.sort((a,b) => {return b-a});}
+      
+      return item;
+    });
+  
+    return  resultArr.flat();
+  }
+  
